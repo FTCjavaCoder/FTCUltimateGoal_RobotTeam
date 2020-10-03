b25e38cd96604d3a8241a6b5d8e4942de5d592ee
Caleb <calebspiess@gmail.com>
2020-10-03 14:38:00 -0700

    Working 6.0 shooter test

--- OpModes/Test/Prototypes/ShooterCalebTest.java
+++ OpModes/Test/Prototypes/ShooterCalebTest.java
@@ -1,41 +1,42 @@
 package UltimateGoal_RobotTeam.OpModes.Test.Prototypes;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import UltimateGoal_RobotTeam.OpModes.TeleOp.BasicTeleOp;
-
 
 @TeleOp(name="Shooter Test Caleb", group="Test")
 
 public class ShooterCalebTest extends BasicTeleOp {
 
+    public DcMotor shooterLeft = null;
+    public DcMotor shooterRight = null;
+
     @Override
     public void runOpMode() {
 
-            initializeTeleOp();
+        shooterLeft = hardwareMap.get(DcMotor.class, "motor_shooterL");
+        shooterRight = hardwareMap.get(DcMotor.class, "motor_shooterR");
 
-            // Wait for the game to start (driver presses PLAY)
+        shooterLeft.setPower(0);
+        shooterRight.setPower(0);
+        shooterLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooterRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooterLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        shooterRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Wait for the game to start (driver presses PLAY)
             waitForStart();
             runtime.reset();
 
         while (opModeIsActive()) {
 
-            if (gamepad1.left_stick_y < -0.25) {
+            shooterLeft.setPower(-gamepad1.left_stick_y);
+            shooterRight.setPower(gamepad1.left_stick_y);
 
-                Billy.shooterLeft.setPower(cons.SHOOTER_POWER_LIMIT);// parameter starts at 0.5
-                Billy.shooterRight.setPower(cons.SHOOTER_POWER_LIMIT);// parameter starts at 0.5
-            }
-            if (gamepad1.left_stick_y >= -0.25) {
-
-                Billy.shooterLeft.setPower(0);
-                Billy.shooterRight.setPower(0);
-            }
-
-            telemetry.addLine("For Blue servo Y to increase position and A to decrease position");
-            telemetry.addLine("For Red servo B to increase position and X to decrease position");
-            telemetry.addData("Motor Power", "Right Power (%.2f), Left Power (%.2f)", Billy.shooterRight.getPower(), Billy.shooterLeft.getPower());
+            telemetry.addData("Motor Power", "Right Power (%.2f), Left Power (%.2f)", shooterRight.getPower(), shooterLeft.getPower());
             telemetry.update();
         }
     }
-
 }
