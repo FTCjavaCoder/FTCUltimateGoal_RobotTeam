9a0a881a000d51b6da3b2d629f99c43f38237ea2
Caleb <calebspiess@gmail.com>
2020-11-20 16:32:47 -0700

    Update WobbleArm.java

--- HarwareConfig/WobbleArm.java
+++ HarwareConfig/WobbleArm.java
@@ -24,6 +24,7 @@ public class WobbleArm {
     public int wobbleArmTarget = 0;
     public int wobbleArmTargetAngle = 0;
     public int armDegInc = 1;
+    public int armDegIncBig = 15;
     public double armPower = 0.25;
     public double powerInc = 0.05;
     public double armPowerHold = 0.7;
@@ -49,60 +50,70 @@ public class WobbleArm {
         }
     }
 
-    public void setWobbleMotorPower(Gamepad gamepad2, BasicOpMode om) {
+    public void setWobbleMotorPower(Gamepad gamepad, BasicOpMode om) {
 
-        if (gamepad2.left_bumper) {
+        if (gamepad.left_bumper) {
             armPower -= powerInc;
             wobbleGoalArm.setPower(armPower);
             om.sleep(300);
         }
-        if (gamepad2.right_bumper) {
+        if (gamepad.right_bumper) {
             armPower += powerInc;
             wobbleGoalArm.setPower(armPower);
             om.sleep(300);
         }
 
-        if (gamepad2.b) {
-            armPower = 0.0;
-            wobbleGoalArm.setPower(armPower);
-            om.sleep(300);
-        }
+//        if (gamepad.b) {
+//            armPower = 0.0;
+//            wobbleGoalArm.setPower(armPower);
+//            om.sleep(300);
+//        }
 
     }
 
-    public void changeWobbleMotorVariable(Gamepad gamepad2, BasicOpMode om) {
+    public void changeWobbleMotorVariable(Gamepad gamepad, BasicOpMode om) {
 
-        if (gamepad2.dpad_up) {
+        if (gamepad.dpad_up) {
             wobbleArmTargetAngle += armDegInc;
             wobbleArmTarget = (int) Math.round(wobbleArmTargetAngle * (cons.DEGREES_TO_COUNTS_60_1 * armGearRatio));
             om.sleep(300);
         }
-        if (gamepad2.dpad_down) {
+        if (gamepad.dpad_down) {
             wobbleArmTargetAngle -= armDegInc;
+            wobbleArmTarget = (int) Math.round(wobbleArmTargetAngle * (cons.DEGREES_TO_COUNTS_60_1 * armGearRatio));
+            om.sleep(300);
+        }
+        if (gamepad.x) {
+            wobbleArmTargetAngle += armDegIncBig;
+            wobbleArmTarget = (int) Math.round(wobbleArmTargetAngle * (cons.DEGREES_TO_COUNTS_60_1 * armGearRatio));
+            om.sleep(300);
+        }
+        if (gamepad.b) {
+            wobbleArmTargetAngle -= armDegIncBig;
             wobbleArmTarget = (int) Math.round(wobbleArmTargetAngle * (cons.DEGREES_TO_COUNTS_60_1 * armGearRatio));
             om.sleep(300);
         }
 
     }
 
-    public void setWobbleMotorPosition(Gamepad gamepad2, BasicOpMode om) {
+    public void setWobbleMotorPosition(Gamepad gamepad, BasicOpMode om) {
 
-        if (gamepad2.dpad_right) {
+        if (gamepad.dpad_right) {
             wobbleGoalArm.setTargetPosition(wobbleArmTarget);
             om.sleep(300);
         }
 
     }
 
-    public void setWobbleServoPos(Gamepad gamepad2, BasicOpMode om) {
+    public void setWobbleServoPos(Gamepad gamepad, BasicOpMode om) {
 
-        if (gamepad2.y) {
+        if (gamepad.y) {
 
             wobbleGoalPos += wobbleGrabInc;
             wobbleGoalServo.setPosition(wobbleGoalPos);
             om.sleep(250);
         }
-        if (gamepad2.a) {
+        if (gamepad.a) {
 
             wobbleGoalPos -= wobbleGrabInc;
             wobbleGoalServo.setPosition(wobbleGoalPos);
